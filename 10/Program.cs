055aac8f1ebd4340202fdb4590276705ce136f34
Шаймарданов Ильфат <ilfatsh@Sajmardanovs-MacBook-Pro.local>
2022-10-11 19:37:18 +0300

    Solved Task 10

---
+++ 10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру числа 
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+
+int num = int.Parse(Console.ReadLine());
+int num1 = num/10;
+int result = num1 % 10;
+
+Console.WriteLine($"Вторая цифра числа {num} -> {result}");
+
