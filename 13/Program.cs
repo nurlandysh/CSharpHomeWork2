8b6a599d43792392bb217559497dc89eae7b7418
Шаймарданов Ильфат <ilfatsh@Sajmardanovs-MacBook-Pro.local>
2022-10-11 19:36:51 +0300

    Solved Task 13

---
+++ 13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу которая выводит третью цифру заданного числа 
+// или сообщает что третьей цифры нет
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine()!);
+int result = 0;
+if (num < 1000 && num >=100)
+{
+    result = num%10;
+    Console.WriteLine($"Третья цифра числа {num} -> {result}");
+}
+else 
+{
+    Console.WriteLine($"Число не трехзначное!");
+}
